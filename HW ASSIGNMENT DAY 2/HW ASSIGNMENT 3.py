86026b912d00fb74ba142c161fe4c555bb0746aa
meghashyam123 <64949388+meghashyam123@users.noreply.github.com>
2020-06-06 18:41:17 +0530

    ASSIGNMENT

---
+++ HW ASSIGNMENT DAY 2/HW ASSIGNMENT 3.py
@@ -0,0 +1,25 @@
+  
+import cv2 
+import time
+import math
+
+cam = cv2.VideoCapture(0)
+counter = 0
+
+while True :
+    counter = counter + 1
+    startTime = time.time()
+    for i in range(0, 5):
+        print(i)
+        time.sleep(2)
+    endTime = time.time()
+    passTime = endTime - startTime
+    print("passTime= %s" % passTime)
+    x , display = cam.read()
+    flipped = cv2.flip(frame,-1)
+    if cv2.waitKey(5) & 0xFF == ord('q') :
+        break
+    if math.floor(passTime) % counter == 0 :
+        cv2.imshow('display',flipped)
+    else :
+        cv2.imshow('display', display)
