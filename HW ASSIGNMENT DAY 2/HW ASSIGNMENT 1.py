3ba221866e0a82c7b95ded1aa32f3496b3e17035
meghashyam123 <64949388+meghashyam123@users.noreply.github.com>
2020-06-06 18:03:46 +0530

    Delete HW ASSIGNMENT 1.py

--- HW ASSIGNMENT DAY 2/HW ASSIGNMENT 1.py
+++
@@ -1,9 +0,0 @@
-import cv2 as cv
-img = cv.imread('tiger.jpg')
-print(img)
-
-print(img.shape)
-cv.line(img,(0,0),(img.shape[1],img.shape[0]),(0,0,0),4)
-cv.line(img,(img.shape[1],0),(0,img.shape[0]),(0,0,0),4)
-cv.imshow('Figure',img)
-cv.waitKey(0)
