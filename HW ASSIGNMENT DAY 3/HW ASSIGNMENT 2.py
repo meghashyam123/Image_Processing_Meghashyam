8b835081012b1672f5c01ac90a1cfe5a1304a9aa
meghashyam123 <64949388+meghashyam123@users.noreply.github.com>
2020-06-10 13:13:35 +0530

    Update HW ASSIGNMENT 2.py

--- HW ASSIGNMENT DAY 3/HW ASSIGNMENT 2.py
+++ HW ASSIGNMENT DAY 3/HW ASSIGNMENT 2.py
@@ -4,27 +4,30 @@ import random
 
 img = cv2.imread ('C:\Git\Image_Processing_Meghashyam\HW ASSIGNMENT DAY 3\tiger.jpg')
 s = img.shape
-y = s[0] / 7
-x= s[1] / 7
+x = s[0] / 7
+y= s[1] / 7
 
-for i in range (1, 8):
-    i1 = int(y * (i-1))
-    i2 = int(y * i)
 
-    if i % 2 == 0:
+for j in range (1,8):
+    j1 = int(x * (x-1))
+    j2 = int(x * x)
+
+    if j % 2 == 0:
         a = 1
         b= 8
         c = 1
-    elif i % 2 != 0:
+    elif j % 2 != 0:
         a = 7
         b = 0
         c = -1
 
-    for j in range (a, b, c):
-        j1 = int(x * (x-1))
-        j2 = int(x * x)
+    for i in range (a,b,c):
+        i1 = int(y * (i-1))
+        i2 = int(y * i)
 
-        img [ i1:i2, j1:j2 ] = (random.randint (0, 255), random.randint (0, 255), random.randint (0, 255))
 
-        cv2.imshow ('display', img)
-        cv2.waitKey(500)
+    img [ i1:i2, j1:j2 ] = (random.randint (0, 255), random.randint (0, 255), random.randint (0, 255))
+
+    cv2.imshow ('display', img)
+    cv2.waitKey(500)
+
