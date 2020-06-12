e77545dff6e194d735e3d8c5b44e2e351606ae93
meghashyam123 <64949388+meghashyam123@users.noreply.github.com>
2020-06-12 12:35:32 +0530

    Update HW ASSIGNMENT 1.py

--- HW ASSIGNMENT DAY 4/HW ASSIGNMENT 1.py
+++ HW ASSIGNMENT DAY 4/HW ASSIGNMENT 1.py
@@ -1,18 +1,17 @@
 import cv2
 import numpy as np 
 
-cam=cv2.VideoCapture(0)
+cap=cv2.VideoCapture(0)
 
 def mouse(event,x,y,flags,param):
-    global points,flag
     if event==cv2.EVENT_LBUTTONDOWN:
         print(display[y,x])
 
-cv2.namedWindow('img')
-cv2.setMouseCallback('img',mouse)
+cv2.namedWindow('cam')
+cv2.setMouseCallback('cam',mouse)
 
 while True:
-    x,display= cam.read()
-    cv2.imshow('img',display)
-    if cv2.waitKey(5000) & 0xFF==ord('q'):
+    x,display= cap.read()
+    cv2.imshow('cam',display)
+    if cv2.waitKey(1) & 0xFF==ord('q'):
         break
